2b335b560891ebc7ecd3cc2b2cca0a4dda84ab28
Mizux Seiha <mizux.dev@gmail.com>
2020-05-06 21:43:23 +0200

    Update indent

--- cmake/sample/main.cpp
+++ cmake/sample/main.cpp
@@ -1,8 +1,8 @@
-#include <foo/Foo.hpp>
 #include <bar/Bar.hpp>
+#include <foo/Foo.hpp>
 
 int main(int argc, char** argv) {
-	foo::hello();
-	bar::hello();
-	return 0;
+  foo::hello();
+  bar::hello();
+  return 0;
 }
