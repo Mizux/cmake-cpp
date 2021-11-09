b17fe047c0bb097efae9a3b5bf326a1c47d1f754
Mizux Seiha <mizux.dev@gmail.com>
2021-11-09 17:09:52 +0100

    ci: Rework sample

--- ci/sample/main.cpp
+++ ci/sample/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include <foo/Foo.hpp>
+#include <bar/Bar.hpp>
 #include <foobar/FooBar.hpp>
 
 int main(int /*argc*/, char** /*argv*/) {
+  foo::fooHello(1);
+  bar::barHello(1);
   foobar::foobarHello(1);
   std::cout << std::endl;
   foobar::FooBar::hello(int{1});
