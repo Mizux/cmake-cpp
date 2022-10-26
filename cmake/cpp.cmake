0d08ed532b9b43e6223633efa6ccf00b0a619ae4
Corentin Le Molgat <corentinl@google.com>
2022-10-26 17:34:18 +0200

    Add googletest

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -35,12 +35,13 @@ endif()
 
 if(BUILD_TESTING)
   include(FetchContent)
-  #FetchContent_Declare(
-  #  googletest
-  #  GIT_REPOSITORY https://github.com/google/googletest.git
-  #  GIT_TAG master)
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG main)
   #set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  #FetchContent_MakeAvailable(googletest)
+  FetchContent_MakeAvailable(googletest)
+
   FetchContent_Declare(
     Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
