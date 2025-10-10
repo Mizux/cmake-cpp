17f3b14b11f3dcc7ba48dab2b216b50e80950a3d
Corentin Le Molgat <corentinl@google.com>
2025-10-10 16:54:30 +0200

    ci: rework samples

--- ci/samples/main.cpp
+++ ci/samples/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include <absl/base/log_severity.h>
+#include <absl/flags/parse.h>
+#include <absl/flags/usage.h>
+#include <absl/log/globals.h>
+#include <absl/log/initialize.h>
+#include <absl/log/log.h>
+#include <absl/strings/str_join.h>
 
 #include <foo/Foo.hpp>
 #include <bar/Bar.hpp>
 #include <foobar/FooBar.hpp>
 
-int main(int /*argc*/, char** /*argv*/) {
+int main(int argc, char* argv[]) {
+  absl::InitializeLog();
+  absl::SetProgramUsageMessage("FooBarApp");
+  absl::EnableLogPrefix(false);
+  absl::SetStderrThreshold(absl::LogSeverity::kInfo);
+  absl::ParseCommandLine(argc, argv);
+  {
+    const std::vector<std::string> v = {"foo","bar","baz"};
+    std::string s = absl::StrJoin(v, "-");
+    LOG(INFO) << "Joined string: " << s << "\n";
+  }
+
   foo::freeFunction(0);
   bar::freeFunction(1);
   foobar::freeFunction(2);
